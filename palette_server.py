80c5eb4bc0352b1ffb973ed30ee5e92215fdf664
root <krinsang@umich.edu>
2018-02-15 05:19:33 +0000

    now it returns five links

--- palette_server.py
+++ palette_server.py
@@ -43,7 +43,6 @@ def classify():
         #image_base64_str = tfpp_json['jpg'].replace('data:image/jpeg;base64,', '')
         f = open("vie.jpg",'r+')
 	img_jpg = f.read()
-	print("what")
         image_content = base64.b64encode(img_jpg)
         res_json = goog_cloud_vison(image_content)
         res_json['description'] = 'Label Detection (Google Cloud Vision)'
@@ -53,7 +52,6 @@ def classify():
 		descriptions [index] = i['description']
 		index += 1
 	descriptions = {'descriptions':descriptions}
-	print("Before searchParses func")
 	return searchParses(descriptions)
 	#return "jpg received"
 
@@ -62,7 +60,6 @@ def searchParses(descriptions):
         print("in searchParses")
         for i in range(3):
                 query += (descriptions['descriptions'][i] + ' ')
-                print(i)
         subscription_key = "e9dbf70ca16c4533932bd31b5bb204bb"
         assert subscription_key
         search_url = "https://api.cognitive.microsoft.com/bing/v7.0/search"
@@ -71,9 +68,14 @@ def searchParses(descriptions):
         response = requests.get(search_url, headers = headers, params = params)
         response.raise_for_status()
         search_results = response.json()
-	print(search_results)
-	return search_results
-
+	webSites = [None] * 5
+	for i in range(5):
+		#search_results['webPages']['value']:
+		#print(i)
+		webSites[i] = (search_results['webPages']['value'][i]['url'])
+	#print(webSites)
+	webSites = {'webSites' : webSites}
+	return jsonify(webSites)
 if __name__ == '__main__':                                 
 	app.run(host="159.65.33.47", port=default_port)
 
