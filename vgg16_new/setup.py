6ecc46146b1ac51f0bf8b82b159037a1b6e5331f
Daniel Hyunsuk Choi <31271953+crechoi@users.noreply.github.com>
2018-03-22 22:11:02 -0400

    Update setup.py

--- vgg16_new/setup.py
+++ vgg16_new/setup.py
@@ -7,3 +7,6 @@ REQUIRED_PACKAGES = [
 	'scipy'
 ]
 setup(name='vgg16_new', version='0.1', install_requires=REQUIRED_PACKAGES, packages=find_packages(), include_package_data=True, description='Classifier test')
+
+
+#gcloud ml-engine jobs submit training image_classification17     --stream-logs     --module-name vgg16_new.train_vgg     --package-path vgg16_new     --staging-bucket $BUCKET_NAME     --region us-east1     --runtime-version=1.4    --     --output_path "${GCS_PATH}/output"     --eval_data_paths "${GCS_PATH}/*"     --train_data_paths "${GCS_PATH}/*"
